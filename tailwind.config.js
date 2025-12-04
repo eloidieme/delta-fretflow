741d30fe291f807ee454bd5e5bc0c22f6d77b96b
Eloi Dieme <eloidieme@gmail.com>
2025-12-04 16:12:12 +0100

    Closes #1

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content:
+      [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+      ],
+  theme: {
+    extend:
+        {
+            // We will add your brand colors and font settings here later
+        },
+  },
+  plugins: [],
+}
